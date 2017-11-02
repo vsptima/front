a819b27d33665d8697fe0bb9637afa754ca0d603
Vasin Semen <vsp.tima@gmail.com>
2017-11-02 19:47:12 +0300

    Сделана менюшка.

--- design/layouts/_menu.php
+++ design/layouts/_menu.php
@@ -12,9 +12,27 @@
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
                 <li class="active"><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li><a href="/design/editor.php" target="_blank">Editor</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Feedback</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Gallery</a></li>
+                        <li><a href="#">Hobby</a></li>
+                        <li><a href="#">Jargon</a></li>
+                        <li><a href="#">Skill</a></li>
+                        <li><a href="#">Term</a></li>
+                        <li><a href="#">Github</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Edit <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Editor</a></li>
+                    </ul>
+                </li>
             </ul>
         </div><!--/.nav-collapse -->
     </div><!-- /.container -->
