920e25b54342ff6c3aec80858f4eb80743d9b571
Vasin Semen <vsp.tima@gmail.com>
2017-11-02 20:23:34 +0300

    Форма + медиа-css

--- design/feedback.php
+++ design/feedback.php
@@ -5,7 +5,29 @@ $pageDescription = 'Description '.$pageTitle;
 <?php require_once 'layouts/_top.php';?>
     <div class="col-md-9 main">
         <h1><?=$pageTitle?></h1>
-        <p><?=$pageDescription?></p>
+        <form class="form-horizontal">
+            <div class="row">
+                <div class="col-md-6">
+                    <label for="email">Your name</label>
+                    <input type="text" class="form-control" name="name" id="name" placeholder="ваше имя">
+                </div>
+                <div class="col-md-6">
+                    <label for="email">Your email address</label>
+                    <input type="email" class="form-control" name="email" id="email" placeholder="ваша электронная почта">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 form-message">
+                    <textarea class="form-control" rows="10" name="message"></textarea>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-md-offset-6 text-right">
+                    <input type="reset" class="btn btn-default" value="Reset">
+                    <input type="submit" class="btn btn-default" value="Submit">
+                </div>
+            </div>
+        </form>
     </div>
     <div class="col-md-3 sidebar">
         <?php require_once 'layouts/_sidebar_index.php';?>
