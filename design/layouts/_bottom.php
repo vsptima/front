88abac04c36bed166b44e7c1176d9152a0b7b024
Vasin Semen <vsp.tima@gmail.com>
2017-11-07 20:18:58 +0300

    Подключение javascript-хранилищ

--- design/layouts/_bottom.php
+++ design/layouts/_bottom.php
@@ -3,6 +3,8 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="/src/ie10-viewport-bug-workaround.js"></script>
+<script src="/src/dataArray.js"></script>
+<script src="/src/dataObject.js"></script>
 <script src="/src/script.js"></script>
 </body>
 </html>
