337a3b2943d55a94f0cf8896862842c81f696098
Vasin Semen <vsp.tima@gmail.com>
2017-11-07 20:59:36 +0300

    SQLite-хранилище + его тестирование

--- design/hobby.php
+++ design/hobby.php
@@ -1,11 +1,16 @@
 <?php
 $pageTitle = 'Хобби';
 $pageDescription = 'Description '.$pageTitle;
+$pdo = new PDO('sqlite:data/hobby');
+$games = $pdo->query("SELECT * FROM games"); // PDO::FETCH_ASSOC
 ?>
 <?php require_once 'layouts/_top.php';?>
     <div class="col-md-9 main">
         <h1><?=$pageTitle?></h1>
         <p><?=$pageDescription?></p>
+        <?php foreach ($games as $game) : ?>
+            <p><?=$game['name']?></p>
+        <?php endforeach; ?>
     </div>
     <div class="col-md-3 sidebar">
         <?php require_once 'layouts/sidebar_hobby.php';?>
