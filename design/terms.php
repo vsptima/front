b14f5ee55c5d1cf26968c8ab27a920e840d49275
Vasin Semen <vsp.tima@gmail.com>
2017-11-07 20:10:19 +0300

    TXT-хранилище

--- design/terms.php
+++ design/terms.php
@@ -1,8 +1,10 @@
 <?php
 $pageTitle = 'Термины';
 $pageDescription = 'Description '.$pageTitle;
+$arrTerms = file('data/term.txt');
 ?>
 <?php require_once 'layouts/_top.php';?>
 <h1><?=$pageTitle?></h1>
 <p><?=$pageDescription?></p>
+<pre><?php print_r($arrTerms);?></pre>
 <?php require_once 'layouts/_bottom.php';?>
