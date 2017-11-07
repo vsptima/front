37862b441d4e7cf9291d2721f42d078789936023
Vasin Semen <vsp.tima@gmail.com>
2017-11-07 20:06:32 +0300

    JSON-хранилище

--- design/github.php
+++ design/github.php
@@ -1,11 +1,14 @@
 <?php
 $pageTitle = 'GitHub';
 $pageDescription = 'Description '.$pageTitle;
+$json = file_get_contents('data/github.json');
+$arrGitHub = json_decode($json,true);
 ?>
 <?php require_once 'layouts/_top.php';?>
     <div class="col-md-9 main">
         <h1><?=$pageTitle?></h1>
         <p><?=$pageDescription?></p>
+        <pre><?php print_r($arrGitHub )?></pre>
     </div>
     <div class="col-md-3 sidebar">
         <?php require_once 'layouts/sidebar_github.php';?>
