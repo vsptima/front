6481d4c653a32b22b4a9b290f9d8a774324656c2
Vasin Semen <vsp.tima@gmail.com>
2017-11-14 20:18:11 +0300

    Вывод содержимого файла term.php

--- design/terms.php
+++ design/terms.php
@@ -4,7 +4,17 @@ $pageDescription = 'Description '.$pageTitle;
 $arrTerms = file('data/term.txt');
 ?>
 <?php require_once 'layouts/_top.php';?>
-<h1><?=$pageTitle?></h1>
-<p><?=$pageDescription?></p>
-<pre><?php print_r($arrTerms);?></pre>
+    <h1><?=$pageTitle?></h1>
+    <p><?=$pageDescription?></p>
+    <div class="row grid">
+        <?php foreach($arrTerms as $val):?>
+            <?php $term = explode(":",$val); ?>
+            <div class="col-md-4 grid-item">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><strong><?=$term[0];?></strong></div>
+                    <div class="panel-body"><?=$term[1];?></div>
+                </div>
+            </div>
+        <?php endforeach;?>
+    </div>
 <?php require_once 'layouts/_bottom.php';?>
