6d9482ba9115e042c35696feb3507d9fb83497dc
Vasin Semen <vsp.tima@gmail.com>
2017-11-16 19:48:33 +0300

    Вывод данных из xml-файла

--- design/skill.php
+++ design/skill.php
@@ -6,6 +6,21 @@ $pageDescription = 'Description '.$pageTitle;
     <div class="col-md-9 main">
         <h1><?=$pageTitle?></h1>
         <p><?=$pageDescription?></p>
+        <div class="row marketing">
+            <?php
+            $xml = simplexml_load_file('data/skill.xml');
+            ?>
+            <?php foreach ($xml as $val):?>
+                <div class="col-md-3">
+                    <div class="panel panel-default">
+                        <div class="panel-heading"><?= (string)$val->title; ?></div>
+                        <div class="panel-body">
+                            <?= (string)$val->content; ?>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach;?>
+        </div>
     </div>
     <div class="col-md-3 sidebar">
         <?php require_once 'layouts/sidebar_skill.php';?>
