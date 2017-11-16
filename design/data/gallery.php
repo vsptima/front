a64e3988586442ed27c944f07029d5f3f604992c
Vasin Semen <vsp.tima@gmail.com>
2017-11-16 19:55:21 +0300

    Заполнение хранилища галлереи

--- design/data/gallery.php
+++ design/data/gallery.php
@@ -1,11 +1,16 @@
 <?php
 return [
     [
-        'url' => '//////',
-        'title' => '//////',
+        'url' => 'http://mgnews.ru/image/normal/14846/warhammer-40000-dark-millenium-online-5.jpg',
+        'title' => 'Космодесантник с орудием',
     ],
     [
-        'url' => '//////',
-        'title' => '//////',
+        'url' => 'https://static1.squarespace.com/static/566de2b3d8af104b6800e718/t/56916c45cbced6b74bc310d0/1452371017511/3292_70031f16cf28e832e6c03d3df01b6931.jpg?format=1500w',
+        'title' => 'Дредноут',
     ],
+    [
+        'url' => 'http://b1.m24.ru/c/304817.483xp.jpg',
+        'title' => 'Космодесантник',
+    ]
+
 ];
