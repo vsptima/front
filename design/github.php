b4ae79af25344d001f20ec340175616e6f8c7937
Vasin Semen <vsp.tima@gmail.com>
2017-11-23 19:55:26 +0300

    Вывод ссылок на Github.

--- design/github.php
+++ design/github.php
@@ -8,7 +8,20 @@ $arrGitHub = json_decode($json,true);
     <div class="col-md-9 main">
         <h1><?=$pageTitle?></h1>
         <p><?=$pageDescription?></p>
-        <pre><?php print_r($arrGitHub )?></pre>
+
+
+        <div class="row grid">
+            <?php foreach ($arrGitHub as $val) : ?>
+                <div class="col-md-3 grid-item">
+                    <div class="panel panel-default">
+                        <div class="panel-heading"><?= $val['title']; ?></div>
+                        <div class="panel-body"><?= $val['description'];?></div>
+                        <div class="panel-footer"><a href="<?= $val['url']; ?>" title="<?= $val['title'];?>">Link</a></div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+
+        </div>
     </div>
     <div class="col-md-3 sidebar">
         <?php require_once 'layouts/sidebar_github.php';?>
