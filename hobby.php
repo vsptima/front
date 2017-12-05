c30a5bfed14e26e50d5ed625604d226f6a4399ca
Vasin Semen <vsp.tima@gmail.com>
2017-12-05 20:07:03 +0300

    Вывод хобби.

--- hobby.php
+++ hobby.php
@@ -8,9 +8,24 @@ $games = $pdo->query("SELECT * FROM games"); // PDO::FETCH_ASSOC
     <div class="col-md-9 main">
         <h1><?=$pageTitle?></h1>
         <p><?=$pageDescription?></p>
-        <?php foreach ($games as $game) : ?>
-            <p><?=$game['name']?></p>
-        <?php endforeach; ?>
+
+        <div class="row">
+            <?php foreach ($games as $game) : ?>
+                <div class="col-md-4">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <a href="/games/waw.html"><?=$game['name']?></a>
+                        </div>
+                        <div class="panel-body">
+                            <a href="/storage/pictures/waw-mid.jpg" class="group1 cboxElement">
+                                <img class="img-thumbnail" src="<?=$game['description']?>" alt="<?=$game['name']?>">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+
     </div>
     <div class="col-md-3 sidebar">
         <?php require_once 'layouts/sidebar_hobby.php';?>
